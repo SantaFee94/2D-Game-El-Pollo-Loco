57f99773654e395498ab0af18405cd283714281b
SantaFee94 <timokosfeld11@gmail.com>
2025-04-07 14:36:56 +0200

    Refactor Character and MovableObject classes to improve image loading structure

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,22 +1,25 @@
 class MovableObject {
-    x = 80;
-    y = 155;
-    img;
-    height = 280;
-    width = 130;
+  x = 80;
+  y = 155;
+  img;
+  height = 280;
+  width = 130;
+  imageCache = {};
 
+  loadImage(path) {
+    this.img = new Image();
+    this.img.src = path;
+  }
 
-    loadImage(path) {
-        this.img = new Image();
-        this.img.src = path;
-        
-    }
+  loadImages(arr) {
+    arr.forEach((path) => {
+      let img = new Image();
+      img.src = path;
+      this.imageCache[path] = path;
+    });
+  }
 
-    moveRight() {
-    
-    }
+  moveRight() {}
 
-    moveLeft(){
-
-    }
+  moveLeft() {}
 }
