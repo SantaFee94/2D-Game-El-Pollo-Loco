dcb5c5568d688b4d33b04ff6a82988ea9c1b570d
SantaFee94 <timokosfeld11@gmail.com>
2025-04-07 14:22:18 +0200

    Implementing the animation method for cloud objects

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -7,5 +7,12 @@ width = 500;
     super().loadImage('img/5_background/layers/4_clouds/1.png');
     this.x = Math.random() * 500;
     
+    this.animate();
+  }
+
+  animate() {
+    setInterval( () => {
+      this.x -= 0.20
+    }, 1000 / 60);
   }
 }
